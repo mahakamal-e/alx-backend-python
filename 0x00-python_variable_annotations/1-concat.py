6a20c011552418dac94997475d9c0c69035b0664
mahakamal-e <mahakamal09@gmail.com>
2024-07-04 23:24:01 +0000

    fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,7 +4,7 @@
 
 def concat(str1: str, str2: str) -> str:
     """
-     a type-annotated function concat that takes a string str1 & a string str2
-     as arguments and returns a concatenated string
-     """
-     return str1 + str2
+    a type-annotated function concat that takes a string str1 & a string str2
+    as arguments and returns a concatenated string
+    """
+    return str1 + str2
