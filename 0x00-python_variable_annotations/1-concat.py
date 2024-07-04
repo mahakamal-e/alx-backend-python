93d50c68a0ca2c2358006e931755dd2c18291eea
mahakamal-e <mahakamal09@gmail.com>
2024-07-04 23:21:56 +0000

    fix error

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,7 +2,7 @@
 """ Define concat function """
 
 
-def concat(str1: str, str1: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """
      a type-annotated function concat that takes a string str1 & a string str2
      as arguments and returns a concatenated string
