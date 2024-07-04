230f7e493a43992e1f8addf254c255a7d16ac10d
mahakamal-e <mahakamal09@gmail.com>
2024-07-04 23:18:19 +0000

    upload first task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Define Add function """
+
+
+def add(a: float, b: float) -> float:
+    """
+    a type-annotated function add that takes a float a and a float b
+    as arguments and returns their sum as a float.
+    """
+    return a + b
