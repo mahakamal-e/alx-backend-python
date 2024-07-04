e1bb559ef223dca77af70595a943d8f362876491
mahakamal-e <mahakamal09@gmail.com>
2024-07-04 23:43:01 +0000

    upload task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,5 +4,5 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """a type-annotated function sum_mixed_list"""
+    """a type-annotated function sum_mixed_list."""
     return sum(mxd_lst)
