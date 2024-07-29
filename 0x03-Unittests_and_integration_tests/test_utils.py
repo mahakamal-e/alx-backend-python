2092725c500ebb500efde85cf528f4ad19bbcde6
mahakamal-e <mahakamal09@gmail.com>
2024-07-29 00:20:53 +0000

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Parameterize a unit test Module"""
+"""Define a unit test Module"""
 import unittest
 from parameterized import parameterized
 from utils import *
+from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -23,6 +24,30 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Define the TestGetJson(unittest.TestCase) class """
+    @patch('requests.get')
+    def test_get_json(self, mock_get):
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+
+        for test_url, test_payload in test_cases:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            data = get_json(test_url)
+
+            mock_get.assert_called_once_with(test_url)
+
+            self.assertEqual(data, test_payload)
+
+            mock_get.reset_mock()
+
 
 if __name__ == '__main__':
     unittest.main()
