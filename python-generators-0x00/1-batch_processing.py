b5190b0a54b6eacb4b518b41bae662ca026852d1
mahakamal-e <mahakamal09@gmail.com>
2025-07-06 23:32:31 +0300

    edit

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -4,12 +4,12 @@ import mysql.connector
 
 
 def stream_users_in_batches(batch_size):
-    """ fetchs users in batches from database """
+    """Fetch users in batches from database"""
     connection = mysql.connector.connect(
         host='localhost',
         user='root',
         password='',
-        database='ALX_prodev'    
+        database='ALX_prodev'
     )
     try:
         cursor = connection.cursor(dictionary=True, buffered=True)
@@ -24,10 +24,12 @@ def stream_users_in_batches(batch_size):
         cursor.close()
         connection.close()
 
-    
 
 def batch_processing(batch_size):
-    """ Process each user and filter based on age """
+    """Process each user and filter based on age"""
+    results = []
     for user in stream_users_in_batches(batch_size):
         if user['age'] > 25:
+            results.append(user)
             print(user)
+    return results
