e7c17dde263c585900c5954aae20a34891bb0c5e
mahakamal-e <mahakamal09@gmail.com>
2025-07-20 21:14:03 +0300

    build serializers for the models

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,25 +1,27 @@
 from rest_framework import serializers
-from .models import User, Conversation, Message
+from .models import CustomUser, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['user_id', 'username', 'email', 'phone_number', 'role', 'created_at']
-
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'role']
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
 
     class Meta:
         model = Message
-        fields = ['message_id', 'sender', 'message_body', 'sent_at']
-
+        fields = ['id', 'sender', 'content', 'timestamp']
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
-        fields = ['conversation_id', 'participants', 'created_at', 'messages']
+        fields = ['id', 'participants', 'messages']
+
+    def get_messages(self, obj):
+        messages = obj.message_set.all()
+        return MessageSerializer(messages, many=True).data
