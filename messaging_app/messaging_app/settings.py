aa879e01d2d865b0a16301daa771b2a05a01590a
mahakamal-e <mahakamal09@gmail.com>
2025-07-27 23:29:25 +0300

    fix settings error

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -50,7 +50,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',  # global default
+    'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    
 }
 
 MIDDLEWARE = [
