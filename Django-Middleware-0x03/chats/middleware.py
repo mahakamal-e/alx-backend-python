3471b074e175e2da8bd14a292f0dba3ed047b590
Maha Kamal <58697984+mahakamal-e@users.noreply.github.com>
2025-08-03 16:06:11 +0300

    Update middleware.py

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from django.http import HttpResponseForbidden
 
 
 logger = logging.getLogger(__name__)
@@ -8,6 +9,7 @@ formatter = logging.Formatter('%(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
+
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -20,3 +22,16 @@ class RequestLoggingMiddleware:
 
         response = self.get_response(request)
         return response
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+
+        if current_hour < 6 or current_hour >= 21:
+            return HttpResponseForbidden("Access to the messaging app is restricted at this time.")
+
+        return self.get_response(request)
