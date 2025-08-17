65d1815e28ab35db38090a23f1ff8d7744a31651
mahakamal-e <mahakamal09@gmail.com>
2025-08-17 19:59:29 +0300

    edit signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Message, MessageHistory
+from .models import Message, MessageHistory, Notification
 
 
 @receiver(pre_save, sender=Message)
@@ -14,6 +14,22 @@ def log_message_edit(sender, instance, **kwargs):
                 edited_by=getattr(instance, 'current_user', None)
             )
             instance.edited = True
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
+
+
+@receiver(post_save, sender=Message)
+def create_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance
+        )
             
 
 @receiver(post_delete, sender=User)
