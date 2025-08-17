29c33a6aa4a1865f3c4d9500ce75f96a38a2bc84
mahakamal-e <mahakamal09@gmail.com>
2025-08-17 19:27:45 +0300

    delete

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+@login_required
+def delete_user(request):
+    user = request.user
+    if request.method == "POST":
+        user.delete()
+        return JsonResponse({"message": "Your account and all associated data have been deleted."})
+    return JsonResponse({"error": "Method not allowed."}, status=400)
+
